accf6eb510688af7c66f0928d6b7e4ae79b82d3b
Dan <dsudenfield@gmail.com>
2019-01-14 14:37:23 -0500

    add gitignore, start on node

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <Login></Login>
   </div>
 </template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,19 +8,19 @@
             <form class="form-signin">
               <div class="form-label-group">
                 <label for="inputEmail">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <span class="error" v-if="emailError">*You must enter an email address</span>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" v-model="email" autofocus>
+                <span class="error" v-if="errors.email">*You must enter a valid email address</span>
               </div>
               <div class="form-label-group">
                 <label for="inputPassword">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                <span class="error" v-if="passwordError">*You must enter an email address</span>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" v-model="password">
+                <span class="error" v-if="errors.password">*You must enter a valid password</span>
               </div>
               <div class="custom-control custom-checkbox mb-3">
                 <input type="checkbox" class="custom-control-input" id="customCheck1">
                 <label class="custom-control-label" for="customCheck1">Remember password</label>
               </div>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" @click="login">Sign in</button>
             </form>
           </div>
         </div>
@@ -29,19 +29,52 @@
   </div>
 </template>
 
+<style lang="scss">
+.error {
+  color: red;
+  font-size: .7rem;
+}
+</style>
+
 <script>
     export default {
         data() {
-            return [
+            return {
                 email: null,
                 password: null,
-                emailError: false,
-                passwordError: false
-            ];
+                errors: {
+                  email: false,
+                  password: false
+                }
+            };
         },
         methods: {
-            login() {
-                
+            login(e) {
+              e.preventDefault();
+              if(!this.validateForm()) {
+                return false;
+              }
+              this.submitForm();
+            },
+            submitForm() {
+
+            },
+            validateForm() {
+              let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              if(!this.password || this.password.length < 6) {
+                this.errors.password = true;
+              } else {
+                this.errors.password = false;
+              }
+              if(!this.email || !re.test(this.email)) {
+                this.errors.email = true;
+              } else {
+                this.errors.email = false;
+              }
+              if(!this.errors.email && !this.errors.password) {
+                return true;
+              }
+              return false;
             }
         }
     }

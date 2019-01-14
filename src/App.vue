5bd78cfeff5c79d1d6ada7018dc32e793bddbbb6
Dan <dsudenfield@gmail.com>
2019-01-13 22:38:43 -0500

    remove vuecli build webpack from scratch

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Login></Login>
+  </div>
+</template>
+
+<style lang="scss">
+h1 {
+    color: red;
+}
+</style>
+
+<script>
+import Login from './components/Login.vue';
+export default {
+    components: {
+       Login 
+    }
+}
+</script>
